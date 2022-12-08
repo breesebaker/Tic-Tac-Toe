92cf3e612d39027680fd36661b5e542903e93b99
Christopher Baker <cbreesebaker@gmail.com>
2022-12-08 15:13:15 -0600

    some, but not all java

--- game.js
+++ game.js
@@ -12,3 +12,47 @@ const youWin = [
   [2, 4, 6],
 ];
 
+let options = ["","","","","","","","",""];
+let playerOne = "X";
+let running = false;
+
+initializeGame();
+
+function initializeGame(){
+  cells.forEach(cell => cell.addEventListener("click", cellClicked))
+  restartBtn.addEventListener("click", restartGame)
+  gameText.textContent = ` It's ${playerOne}'s turn!`;
+  running = true;
+}
+function cellClicked(){
+  const cellIndex = this.getAttribute("cellIndex");
+  if(options[cellIndex]!="" || !running){
+    return;
+  }
+
+  updateCell(this, cellIndex);
+  changePlayer();
+  checkWinner();
+}
+function updateCell(cell, index){
+  options[index]=playerOne;
+  cell.gameText = playerOne;
+  
+}
+function changePlayer(){
+  playerOne = (playerOne == "X") ? "O" : "X";
+  statusText.gameText = `It's ${playerOne}'s turn!`
+  
+}
+function checkWinner(){
+  let gameWin = false;
+  for(let i=0;i<youWin.length; i++){
+    const condition = youWin[i];
+    
+  }
+  
+}
+function restartGame(){
+  
+}
+
